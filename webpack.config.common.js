4068c54c22fe8c1544ff70955039e6ea8b3d564f
Neutron <james@neushul.net>
2019-06-25 14:29:20 -0700

    JDN

--- webpack.config.common.js
+++ webpack.config.common.js
@@ -25,7 +25,7 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: './assets/index.html'
+            template: require.resolve('./assets/index.html')
         })
     ]
 };
